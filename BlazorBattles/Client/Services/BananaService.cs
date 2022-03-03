5869fc9ee2bf4eedaca21b8ff95472f862d2f946
Rafal <n1troov4@gmail.com>
2022-03-03 15:08:37 +0100

    xd

---
+++ BlazorBattles/Client/Services/BananaService.cs
@@ -0,0 +1,26 @@
+﻿namespace BlazorBattles.Client.Services
+{
+    public class BananaService : IBananaService
+    {
+        public int Bananas { get; set; } = 1000;
+
+        public event Action OnChange;
+
+        public void EatBananas(int amount)
+        {
+            Bananas -= amount;
+            BananasChanged();
+        }
+
+        public void AddBananas(int amount)
+        {
+            Bananas += amount;
+            BananasChanged();
+        }
+        public void BananasChanged()
+        {
+            OnChange.Invoke();
+        }
+
+    }
+}
---
+++ BlazorBattles/Client/Services/UnitService.cs
@@ -0,0 +1,22 @@
+﻿using BlazorBattles.Shared;
+
+namespace BlazorBattles.Client.Services
+{
+    public class UnitService : IUnitService
+    {
+        public IList<Unit> Units => new List<Unit>
+        {
+            new Unit { Id = 1, Titie = "Knight", Attack = 10, Defense = 10, BananaCost = 100 },
+            new Unit { Id = 2, Titie = "Archer", Attack = 15, Defense = 5, BananaCost = 150 },
+            new Unit { Id = 3, Titie = "Mage", Attack = 20, Defense = 1, BananaCost = 200 },
+        };
+
+        public IList<UserUnit> MyUnits { get; set; } =  new List<UserUnit>();
+
+        public void AddUnit(int unitId)
+        {
+            var unit = Units.First(unit => unit.Id == unitId);  
+            MyUnits.Add(new UserUnit { UnitId = unit.Id , HitPoints = unit.HitPoints});
+        }
+    }
+}
