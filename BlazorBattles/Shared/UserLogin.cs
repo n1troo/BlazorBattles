f12a6126b4103411f6a8a430fff2aa7b6a9c3f0a
Rafal <n1troov4@gmail.com>
2022-04-11 09:19:56 +0200

    xD

---
+++ BlazorBattles/Shared/UserLogin.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorBattles.Shared;
+
+public class UserLogin
+{
+    [Required]
+    public string UserName { get; set; }
+    [Required]
+    public string Password { get; set; }
+}
