c774f4afcd47c01f8078d472478e208ed09a68f9
Rafal <n1troov4@gmail.com>
2022-04-20 15:09:40 +0200

    API controler unit GET

--- BlazorBattles/Client/Services/IUnitService.cs
+++ BlazorBattles/Client/Services/IUnitService.cs
@@ -6,9 +6,10 @@ namespace BlazorBattles.Client.Services
 {
     public interface IUnitService
     {
-        IList<Unit> Units { get; }
+        IList<Unit>? Units { get; set; }
         IList<UserUnit> MyUnits { get; set; }
         void AddUnit(int unitId);
-        
+
+        Task LoadUnitsAsync();
     }
 }
--- BlazorBattles/Client/Services/UnitService.cs
+++ BlazorBattles/Client/Services/UnitService.cs
@@ -1,4 +1,5 @@
-﻿using BlazorBattles.Shared;
+﻿using System.Net.Http.Json;
+using BlazorBattles.Shared;
 using Blazored.Toast.Services;
 
 namespace BlazorBattles.Client.Services
@@ -6,26 +7,32 @@ namespace BlazorBattles.Client.Services
     public class UnitService : IUnitService
     {
         private readonly IToastService _toastService;
+        private readonly HttpClient _httpClient;
 
-        public UnitService(IToastService toastService)
+        public UnitService(IToastService toastService, HttpClient httpClient)
         {
             _toastService = toastService;
+            _httpClient = httpClient;
         }
-        public IList<Unit> Units => new List<Unit>
-        {
-            new Unit { Id = 1, Titie = "Knight", Attack = 10, Defense = 10, BananaCost = 100 },
-            new Unit { Id = 2, Titie = "Archer", Attack = 15, Defense = 5, BananaCost = 150 },
-            new Unit { Id = 3, Titie = "Mage", Attack = 20, Defense = 1, BananaCost = 200 },
-        };
 
+        public IList<Unit>? Units { get; set; } = new List<Unit>();
         public IList<UserUnit> MyUnits { get; set; } =  new List<UserUnit>();
 
         public void AddUnit(int unitId)
         {
+            
             var unit = Units.First(unit => unit.Id == unitId);  
             MyUnits.Add(new UserUnit { UnitId = unit.Id , HitPoints = unit.HitPoints});
          
             _toastService.ShowSuccess($"Unit {unit.Titie} added!");
+        }
+
+        public async Task LoadUnitsAsync()
+        {
+            if (Units.Count == 0)
+            {
+                Units = await _httpClient.GetFromJsonAsync<List<Unit>>("/api/unit");
+            }
         }
     }
 }
